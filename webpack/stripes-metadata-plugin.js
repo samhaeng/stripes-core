061deecae2c6c7416a36d3ae9b5fd0822724d878
Rasmus Wølk <rasmuswoelk@gmail.com>
2018-02-01 22:49:06 +0100

    hmm..

--- webpack/stripes-metadata-plugin.js
+++ webpack/stripes-metadata-plugin.js
@@ -5,12 +5,16 @@ const path = require('path');
 const VirtualModulesPlugin = require('webpack-virtual-modules');
 const loadDefaults = require('./loadDefaults');
 const serialize = require('serialize-javascript');
+const dummyRequire = (v) => v;
 
 function processIcons(icons, compiler) {
   return Object.keys(icons).map(key => {
     const fileName = icons[key].fileName;
     const filePath = require.resolve(path.join(compiler.context, 'node_modules', '@folio/users', `/icons/${fileName}`));
-    return icons[key];
+
+    return Object.assign(icons[key], {
+      url: dummyRequire(filePath),
+    });
   });
 }
 
@@ -18,7 +22,7 @@ function parseStripesModules(enabledModules, compiler, alias) {
   return Object.keys(enabledModules).reduce((acc, moduleName) => {
     // console.log('load defaults', loadDefaults(context, moduleName, alias));
     const stripes = loadDefaults(compiler.context, moduleName, alias).stripes;
-    return Object.assign(acc, { [moduleName.replace('@folio/', '')]: Object.assign(stripes, {
+    return Object.assign(acc, { [moduleName]: Object.assign(stripes, {
       icons: processIcons(stripes.icons, compiler),
     }) });
   }, {});
@@ -35,17 +39,21 @@ module.exports = class StripesConfigPlugin {
     // const what = require.resolve(path.join(compiler.options.resolve.alias['@folio/users'], 'package.json'));
 
     // console.log('what', what);
+    // ${require('/Users/rasmuswoelk/Projects/_folio/ui-users/icons/users.png')}
     const users = load['@folio/users'];
     // const icons = users.icons;
-    // console.log('Users', users);
+    // // console.log('Users', users);
     // console.log('Icons', icons);
     // Create a virtual module for Webpack to include in the build
+    // const image = require('/Users/rasmuswoelk/Projects/_folio/ui-users/icons/users.png');
     const stripesVirtualModule = `
-      export = ${serialize(users, { space: 2 })};
+      export default require('/Users/rasmuswoelk/Projects/_folio/ui-users/icons/users.png')
     `;
+    // export default ${JSON.stringify(users)};
+    console.log(JSON.stringify(users));
 
     compiler.apply(new VirtualModulesPlugin({
-      'node_modules/stripes-metadata.json': stripesVirtualModule,
+      'node_modules/stripes-metadata.js': stripesVirtualModule,
     }));
   }
 };
